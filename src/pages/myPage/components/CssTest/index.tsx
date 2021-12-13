b426a02a6d5ad077b57481fc4fb9701991652da6
longwei <1786082175@qq.com>
2021-12-13 21:08:50 +0800

    feature_css选择器

--- src/pages/myPage/components/CssTest/index.tsx
+++ src/pages/myPage/components/CssTest/index.tsx
@@ -10,24 +10,33 @@ function CssTest(props: Props) {
     count: modal.count,
   }));
 
+  const testData = new Array(10).fill('1');
+
+  console.log(testData)
+
   return (
     <div className={styles.cssWrap}>
       <div style={{ margin: 20, display: 'flex', justifyContent: 'space-between' }}>
         <div>useModal全局状态测试：{count}</div>
         <div className={styles.lessLoop}>测试less循环</div>
         <div className={styles.beforeAfter}>测试伪元素before/after</div>
+
+      </div>
+      <div className={styles.mySelector}>
+        {
+          testData.map((item, index) => index % 2 === 0 ? <p className={styles.mySelectorItemEven}>{index}</p> : <p className={styles.mySelectorItemOdd}>{index}</p>)
+        }
       </div>
 
       <div className={styles.myTransfrom}>
-
         <div className={styles.myFixed}>
-
         </div>
       </div>
 
       <div className={styles.top}></div>
       <div className={styles.center}></div>
       <div className={styles.right}></div>
+
     </div>
   );
 }
