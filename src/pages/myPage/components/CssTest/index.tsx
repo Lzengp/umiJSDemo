ae15a582cdd9da18cf65e9d3d38ff4e69204907b
longwei <1786082175@qq.com>
2022-01-08 20:56:32 +0800

    feature_css样式添加动画和进度条

--- src/pages/myPage/components/CssTest/index.tsx
+++ src/pages/myPage/components/CssTest/index.tsx
@@ -1,3 +1,5 @@
+import { Progress } from 'antd';
+import { useEffect, useState } from 'react';
 import { useModel } from 'umi';
 import styles from './index.less';
 
@@ -5,6 +7,22 @@ interface Props {}
 
 function CssTest(props: Props) {
   const {} = props;
+
+  const [percent, setPercent] = useState<number>(0); // 进度
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setPercent((percent) => {
+        if (percent === 100) {
+          return (percent = 0);
+        }
+        return (percent += 10);
+      });
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   const { count } = useModel('project', (modal) => ({
     count: modal.count,
@@ -18,23 +36,50 @@ function CssTest(props: Props) {
         <div>useModal全局状态测试：{count}</div>
         <div className={styles.lessLoop}>测试less循环</div>
         <div className={styles.beforeAfter}>测试伪元素before/after</div>
-
       </div>
       <div className={styles.mySelector}>
-        {
-          testData.map((item, index) => index % 2 === 0 ? <p className={styles.mySelectorItemEven} key={index}>{index}</p> : <p className={styles.mySelectorItemOdd}  key={index}>{index}</p>)
-        }
+        {testData.map((item, index) =>
+          index % 2 === 0 ? (
+            <p className={styles.mySelectorItemEven} key={index}>
+              {index}
+            </p>
+          ) : (
+            <p className={styles.mySelectorItemOdd} key={index}>
+              {index}
+            </p>
+          ),
+        )}
       </div>
 
       <div className={styles.myTransfrom}>
-        <div className={styles.myFixed}>
-        </div>
+        <div className={styles.myFixed} />
       </div>
 
-      <div className={styles.top}></div>
-      <div className={styles.center}></div>
-      <div className={styles.right}></div>
+      <div style={{ display: 'flex' }}>
+        <div>
+          <div className={styles.top} />
+          <div className={styles.center} />
+          <div className={styles.right} />
+        </div>
 
+        <div style={{ marginLeft: '100px' }}>
+          <div style={{ marginTop: '-20px' }}>动画效果</div>
+          <div className={styles.animationStyle} />
+        </div>
+
+        <div style={{ marginLeft: '100px', width: '300px' }}>
+          <div style={{ marginTop: '-20px' }}>进度条</div>
+          <Progress percent={percent} />
+          <Progress
+            type="circle"
+            percent={percent}
+            strokeColor={{
+              '0%': '#108ee9',
+              '100%': '#87d068',
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
