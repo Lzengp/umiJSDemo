32d5b151927e59a308d7a5b95760fbb8fc5e9a7d
longwei <1786082175@qq.com>
2022-06-16 00:33:40 +0800

    添加视频播放，视频截屏，分页打印功能

--- src/pages/myPage/components/VideoAndPrint/index.tsx
+++ src/pages/myPage/components/VideoAndPrint/index.tsx
@@ -1,11 +1,14 @@
-import { Button, message, Modal } from 'antd';
+import { DeleteOutlined, EyeOutlined } from '@ant-design/icons';
+import { Button, message, Modal, Upload } from 'antd';
 import { useState } from 'react';
+import styles from './index.less';
 
 const VideoAndPrint = () => {
   const [imgList, setImgList] = useState<Array<string>>([]);
   const [previewVisible, setPreviewVisible] = useState<boolean>(false);
   const [previewImg, setPreviewImg] = useState<string>();
 
+  // 新开一个窗口进行打印，不太美观
   const print = () => {
     if (!imgList || imgList.length === 0) {
       message.error(`请截屏之后再打印`);
@@ -28,6 +31,10 @@ const VideoAndPrint = () => {
   };
 
   const captureVideo = () => {
+    if (Array.isArray(imgList) && imgList.length >= 12) {
+      message.info('最多截屏12次');
+      return;
+    }
     const video = document.getElementById('myVideo');
     let canvas = document.createElement('canvas');
     let ctx = canvas.getContext('2d');
@@ -38,7 +45,10 @@ const VideoAndPrint = () => {
     const list = [...imgList];
     list.push(base64Str);
     setImgList(list);
-    console.log(dataURLtoFile(base64Str, '图片1.png'));
+    const file = dataURLtoFile(base64Str, '图片1.png');
+    const formData = new FormData();
+    formData.append('file', file);
+    console.log(formData, file);
   };
 
   const dataURLtoFile = (dataurl, filename) => {
@@ -53,24 +63,56 @@ const VideoAndPrint = () => {
     return new File([u8arr], filename, { type: mime });
   };
 
+  // 推荐使用iframe打印
+  function doPrint3() {
+    //判断iframe是否存在，不存在则创建iframe
+    var iframe = document.getElementById('print-iframe');
+    if (!iframe) {
+      var el = document.getElementById('printHtml');
+      iframe = document.createElement('IFRAME');
+      var doc = null;
+      iframe.setAttribute('id', 'print-iframe');
+      iframe.setAttribute(
+        'style',
+        'position:absolute;width:0px;height:0px;left:-500px;top:-500px;',
+      );
+      document.body.appendChild(iframe);
+      doc = iframe.contentWindow.document;
+      //这里可以自定义样式
+      //doc.write("<LINK rel="stylesheet" type="text/css" href="css/print.css">");
+      doc.write('<div>' + el.innerHTML + '</div>');
+      doc.close();
+      iframe.contentWindow.focus();
+    }
+    iframe.contentWindow.print();
+    if (navigator.userAgent.indexOf('MSIE') > 0) {
+      document.body.removeChild(iframe);
+    }
+  }
+
   return (
-    <>
+    <div className={styles.videoAndPrintWrap}>
       <video
         id="myVideo"
-        class="myVideo video-js vjs-default-skin vjs-big-play-centered"
-        controls
-        preload="auto"
+        // class="myVideo video-js vjs-default-skin vjs-big-play-centered"
+        controls // 如果出现该属性，则向用户显示控件，比如播放按钮
+        preload="auto" // 视频在页面加载时进行加载，并预备播放。如果使用 "autoplay"，则忽略该属性。
+        muted // 视频的音频输出为静音
         width="640"
         height="264"
-        data-setup="{}"
-        webkit-playsinline="true"
-        playsinline="true"
-        x-webkit-airplay="true"
-        x5-video-player-type="h5"
-        x5-video-player-fullscreen="true"
-        x5-video-ignore-metadata="true"
+        // autoplay="autoplay" // 视频在就绪后马上播放
+        // loop="loop" // 当媒介文件完成播放后再次开始播放
+        // data-setup="{}"
+        // webkit-playsinline="true"
+        // playsinline="true"
+        // x-webkit-airplay="true"
+        // x5-video-player-type="h5"
+        // x5-video-player-fullscreen="true"
+        // x5-video-ignore-metadata="true"
+        src="/beixuan.mp4"
+        type="video/mp4"
       >
-        <source src="/beixuan.mp4" type="video/mp4" />
+        {/* <source src="/beixuan.mp4" type="video/mp4" /> */}
       </video>
 
       <Button
@@ -78,11 +120,12 @@ const VideoAndPrint = () => {
         onClick={() => {
           captureVideo();
         }}
+        style={{ marginLeft: '20px' }}
       >
         截屏
       </Button>
 
-      <Button type="primary" onClick={() => print()}>
+      <Button type="primary" style={{ margin: '0 20px' }} onClick={() => doPrint3()}>
         打印
       </Button>
 
@@ -95,20 +138,45 @@ const VideoAndPrint = () => {
       </Button>
 
       <div className="print-content" id="printHtml">
-        {(imgList || []).map((item) => (
+        {(imgList || []).map((item, index) => (
           <div
             style={{
               pageBreakBefore: 'always',
               width: '300px',
               height: '300px',
-              cursor: 'pointer',
-            }}
-            onClick={() => {
-              setPreviewVisible(true);
-              setPreviewImg(item);
+              marginRight: '20px',
+              position: 'relative',
+              // display: 'inline-block',
             }}
           >
-            <img src={item} style={{ width: '300px', height: '300px' }} />
+            <pre>
+              <span className={styles.imgActionIcon} id="imgActionIcon">
+                <EyeOutlined
+                  onClick={() => {
+                    setPreviewVisible(true);
+                    setPreviewImg(item);
+                  }}
+                />
+                <DeleteOutlined
+                  onClick={() => {
+                    const list = [...imgList];
+                    list.splice(index, 1);
+                    setImgList(list);
+                  }}
+                />
+              </span>
+            </pre>
+
+            <img
+              className={styles.itemImgInfo}
+              src={item}
+              style={{
+                width: '300px',
+                height: '300px',
+                border: '1px solid #c9c9c9',
+                marginTop: '10px',
+              }}
+            />
           </div>
         ))}
       </div>
@@ -124,7 +192,7 @@ const VideoAndPrint = () => {
       >
         <img alt="example" style={{ width: '100%' }} src={previewImg} />
       </Modal>
-    </>
+    </div>
   );
 };
 
